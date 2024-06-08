0bd4d3d2f21aa90eaa5aa84641f42ac38a88b3e6
Anna Turkiewicz <turkiewicz293@gmail.com>
2024-06-08 11:49:09 -0600

    Add PCA

--- functions.py
+++ functions.py
@@ -8,6 +8,8 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.utils._testing import ignore_warnings
 from sklearn.exceptions import ConvergenceWarning
 
+import datetime
+
 
 def model_perform(model, model_name, params, X_train, y_train, X_test, y_test):
 
@@ -16,21 +18,23 @@ def model_perform(model, model_name, params, X_train, y_train, X_test, y_test):
                              scoring='neg_mean_squared_error',
                              cv=10,
                              verbose=0, n_jobs=-1)
-    #list(deci_tree_gridsearch.get_params().keys())
-        
+    #list(gridsearch.get_params().keys())
+    start = datetime.datetime.now()
     gridsearch.fit(X_train, y_train)
     print('\nBest hyperparameters:', gridsearch.best_params_)
     best_model = gridsearch.best_estimator_
-
-    metrics_dataframe = calculate_metrics(best_model, model_name, X_train, y_train, X_test, y_test)
+    end = datetime.datetime.now()
+    time = (end - start).microseconds
+    #print(list(gridsearch.get_params().keys()))
+    metrics_dataframe = calculate_metrics(best_model, model_name, X_train, y_train, X_test, y_test, time)
     return metrics_dataframe
 
-metrics_dataframe = pd.DataFrame(columns = ['Model', 'R^2', 'MAPE', 'RMSE'])
+metrics_dataframe = pd.DataFrame(columns = ['Model', 'R^2', 'MAPE', 'RMSE', 'Time'])
 models = []
 models_names = []
 predictions_proba_list = []
 
-def calculate_metrics(model, name, X_train, y_train, X_checked, y_checked):
+def calculate_metrics(model, name, X_train, y_train, X_checked, y_checked, time):
     models.append(model)
     models_names.append(name)
     global metrics_dataframe
@@ -44,6 +48,6 @@ def calculate_metrics(model, name, X_train, y_train, X_checked, y_checked):
     MAPE = mean_absolute_percentage_error(y_checked, predictions)
     RMSE = np.sqrt(mean_squared_error(y_checked, predictions))
 
-    metrics_dataframe = pd.concat([metrics_dataframe, pd.DataFrame.from_records([{'Model': name, 'R^2': r, 'MAPE': MAPE, 'RMSE': RMSE}])])
+    metrics_dataframe = pd.concat([metrics_dataframe, pd.DataFrame.from_records([{'Model': name, 'R^2': r, 'MAPE': MAPE, 'RMSE': RMSE, 'Time' : time}])])
 
     return metrics_dataframe
